85e2fe027566f7a433407966712ee01b900c72fb
Benazir annisa muthalib <you@example.com>
2024-07-31 11:59:48 +0800

    Tugas selesai

--- kode1.cpp
+++ kode1.cpp
@@ -1,7 +1,20 @@
 #include <iostream>
 using namespace std;
 
+void cetakdata(string nama, string nisn, string jurusan, int tinggibadan)
+{
+    cout << "Nama Anda: " << nama << endl;
+    cout << "NISN Anda: " << nisn << endl;
+    cout << "Jurusan Anda: " << jurusan << endl;
+    cout << "Tinggi Badan: " << tinggibadan << endl;
+}
+
 int main() {
-    cout << "Selamat Datang di SMK TI Airlangga" << endl;
+    string nama = "Benazzir anisa muthalib";
+    string nisn = "092031934";
+    string jurusan = "PPLG";
+    int tinggibadan = 158;
+
+    cetakdata(nama, nisn, jurusan,tinggibadan);
     return 0;
 }
